97de0d88dbed3ff6e43862daf844573c5d157109
Calvin Ardi <calvin@isi.edu>
2018-02-21 13:29:22 -0800

    new region: eu-west-3

--- pull.sh
+++ pull.sh
@@ -48,7 +48,7 @@ export DATA_DIR=data
 #
 # `ec2-describe-regions` does take some time, so it's easier to
 # update the variable when it has changed
-REGIONS=( ap-south-1 eu-west-2 eu-west-1 ap-northeast-2 ap-northeast-1 sa-east-1
+REGIONS=( ap-south-1 eu-west-3 eu-west-2 eu-west-1 ap-northeast-2 ap-northeast-1 sa-east-1
     ca-central-1 ap-southeast-1 ap-southeast-2 eu-central-1 us-east-1 us-east-2
     us-west-1 us-west-2 )
 
