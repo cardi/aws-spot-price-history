c45e74e14d99de392ddd099924f271033a8c4842
Calvin Ardi <calvin@isi.edu>
2022-01-10 18:54:10 -0800

    pull.sh: reformat bash, support some compressed files

--- pull.sh
+++ pull.sh
@@ -33,11 +33,11 @@ export JAVA_HOME=/usr/lib/jvm/java
 #   unzip -q ec2-api-tools.zip
 #   ln -s ec2-api-tools-*/ ec2-api-tools
 #
-export EC2_HOME=`pwd`/ec2-api-tools
+export EC2_HOME=$(pwd)/ec2-api-tools
 
 # replace keys with your own, or drop them in the following files
-export AWS_ACCESS_KEY=`cat aws_access.private`
-export AWS_SECRET_KEY=`cat aws_secret.private`
+export AWS_ACCESS_KEY=$(cat aws_access.private)
+export AWS_SECRET_KEY=$(cat aws_secret.private)
 
 # where do you want the data?
 export DATA_DIR=data
@@ -54,13 +54,13 @@ REGIONS=( eu-north-1 ap-south-1 eu-west-3 eu-west-2 eu-west-1 ap-northeast-3 ap-
 
 TS="date -Iseconds"
 
+echo "`$TS` starting data pull on $(hostname -f)"
+
 # check if data directory exists
-if [ -d "$DATA_DIR" ];
-then
+if [ -d "$DATA_DIR" ]; then
     # $DATA_DIR exists and is a directory (or a symlink to one), cd into it
     cd $DATA_DIR
-elif [ ! -e "$DATA_DIR" ];
-then
+elif [ ! -e "$DATA_DIR" ]; then
     # nothing named $DATA_DIR exists, mkdir then cd into it
     mkdir $DATA_DIR
     cd $DATA_DIR
@@ -72,29 +72,47 @@ fi
 
 # we could parallelize this loop, but it's best to naturally throttle
 # so we don't unduly burden their servers
-for region in "${REGIONS[@]}"
-do
+for region in "${REGIONS[@]}"; do
     echo "`$TS` $region starting"
 
     # do we already have data?
     DATA_NEWEST="data.$region.newest"
-    if [ -e $DATA_NEWEST ];
-    then
-        # grab the most recent entry timestamp (e.g., 2017-02-02T11:45:34-0800)
-        NEWEST_TS=`head -20 $DATA_NEWEST | awk -F'\t' '{print $3}' | sort -r | head -1`
-        echo "`$TS` $region existing data found: most recent timestamp is $NEWEST_TS"
+    if [ -e $DATA_NEWEST ]; then
+        # TODO check if symlink points to a compressed filename and handle appropriately
+        DATA_NEWEST_RESOLVED=$(readlink ${DATA_NEWEST})
+        if [ "${DATA_NEWEST_RESOLVED: -3}" == ".xz" ]; then
+          # file is compressed
+          # grab the most recent entry timestamp (e.g., 2017-02-02T11:45:34-0800)
+          NEWEST_TS=$(xzcat ${DATA_NEWEST} | head -20 | awk -F'\t' '{print $3}' | sort -r | head -1)
+        else
+          # file is not compressed
+          # grab the most recent entry timestamp (e.g., 2017-02-02T11:45:34-0800)
+          NEWEST_TS=$(head -20 ${DATA_NEWEST} | awk -F'\t' '{print $3}' | sort -r | head -1)
+        fi
+
+        # TODO check if timestamp is well-formatted
+
+        # sometimes datafile is empty
+        if [ -z "$NEWEST_TS" ]; then
+          echo "`$TS` $region existing data found, but most recent timestamp is empty? continuing as if no existing data found"
+          unset NEWEST_TS
+        else
+          echo "`$TS` $region existing data found: most recent timestamp is $NEWEST_TS"
+        fi
     else
         unset NEWEST_TS
-        echo "`$TS` $region no existing data found, starting from the beginning (otherwise symlink 'data.$region.newest' to the most recent file)"
-        echo "`$TS` $region note: the initial data pull can take a while (~1 hour)"
+    fi
+
+    if [ -z "$NEWEST_TS" ]; then
+      echo "`$TS` $region no existing data found, starting from the beginning (otherwise symlink 'data.$region.newest' to the most recent file)"
+      echo "`$TS` $region note: the initial data pull can take a while (~1 hour)"
     fi
 
     # we can only grab the last 90 days of data: if the most recent timestamp is
     # more than 90 days ago then we're essentially starting from scratch.
-    FN_TEMP=data.$region.`date -Iseconds`.temp
+    FN_TEMP=data.$region.$(date -Iseconds).temp
     echo "`$TS` $region starting data refresh"
-    if [ ! -z "$NEWEST_TS" ];
-    then
+    if [ ! -z "$NEWEST_TS" ]; then
         # only get the diff, but there will be some overlap regardless
         $EC2_HOME/bin/ec2-describe-spot-price-history \
             --region $region \
@@ -108,13 +126,12 @@ do
 
     # if the data refresh is finished, then move the files around and update
     # symlinks.
-    FN=data.$region.`date -Iseconds`
-    if [ $? -eq 0 ];
-    then
+    FN=data.$region.$(date -Iseconds)
+    if [ $? -eq 0 ]; then
         mv $FN_TEMP $FN # filename (e.g., data.us-east-1.2017-01-01T01:01:01-0800)
         ln -sfn $FN data.$region.newest # update symlink
 
-        NEWEST_TS=`head -20 $DATA_NEWEST | awk -F'\t' '{print $3}' | sort -r | head -1`
+        NEWEST_TS=$(head -20 $DATA_NEWEST | awk -F'\t' '{print $3}' | sort -r | head -1)
         echo "`$TS` $region finished. most recent entry is $NEWEST_TS."
     else
         echo "`$TS` $region something went wrong."
